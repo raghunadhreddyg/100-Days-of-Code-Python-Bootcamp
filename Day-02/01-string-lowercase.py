1a86c8f333db4a685c7cf8731b3d81bc6f85c84b
reddy007gongati <reddy007gongati@gmail.com>
2025-02-24 01:59:10 -0600

    add new case

---
+++ Day-02/01-string-lowercase.py
@@ -0,0 +1,9 @@
+"""
+This is to convert a text into lower and upper case alphabets.
+"""
+
+text = "Python is awesome"
+uppercase = text.upper()
+lowercase = text.lower()
+print("Uppercase:", uppercase)
+print("Lowercase:", lowercase)
