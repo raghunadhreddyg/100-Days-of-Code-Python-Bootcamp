85e045798b28bd43ced3554cbd1dc8571f4e63a9
reddy007gongati <reddy007gongati@gmail.com>
2025-02-24 02:35:49 -0600

    more operations

---
+++ Day-02/02-float.py
@@ -0,0 +1,26 @@
+"""
+This is to perform basic Arithmetic operations on float numbers.
+"""
+
+#Float Variables
+
+num1 = 5.0
+num2 = 3.5
+
+# Basic Arithmetic operations
+
+result1 = num1 + num2
+print("The addition of given two numbers is", result1)
+
+result2 = num1 - num2
+print("The subtraction of given two numbers is", result2)
+
+result3 = num1 * num2
+print("The multiplication of given two numbers is", result3)
+
+result4 = num1 / num2
+print("The division of given two numbers is", result4)
+
+#Rounding numbers to particular decimal places
+result5 = round(5.1324563, 2)  #Rounds to 2 decimal places
+print("The rounded of number is", result5)
